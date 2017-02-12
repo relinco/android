1abf222167c8dc382598720c14650771f74146a6
relinco <reincoin@gmail.com>
2017-02-12 11:19:51 +0800

    update

--- ud839_Miwok-a6df7e73966c65594ae4c35c89b3e691d0fe9c44/app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ ud839_Miwok-a6df7e73966c65594ae4c35c89b3e691d0fe9c44/app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -3,15 +3,24 @@ package com.example.android.miwok;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.LinearLayout;
+
+import java.util.ArrayList;
 
 public class NumbersActivity extends AppCompatActivity {
-    String [] engNumbers = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"};
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
-        
+        ArrayList<String> engNumbers = new ArrayList<String >();
+        String [] engNum = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"};
+        for(int i = 0; i < engNum.length; i++)
+            engNumbers.add(engNum[i]);
+
+        LinearLayout colorsActivity = (LinearLayout) findViewById(R.id.activity_numbers);
+
+
     }
 }
