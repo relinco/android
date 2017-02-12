f81942a7a6db3647321b0d12e545c04a0c1158c5
relinco <reincoin@gmail.com>
2017-02-12 09:30:57 +0800

    add a array

--- ud839_Miwok-a6df7e73966c65594ae4c35c89b3e691d0fe9c44/app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ ud839_Miwok-a6df7e73966c65594ae4c35c89b3e691d0fe9c44/app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -2,12 +2,17 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class NumbersActivity extends AppCompatActivity {
+    String [] engNumbers = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"};
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+        for (int i = 0; i < engNumbers.length; i++)
+            Log.i("NumbersActivity", "word at index " + i + " : " + engNumbers[i]);
     }
 }
